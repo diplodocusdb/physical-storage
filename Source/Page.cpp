c84cc426dd33fbe6b61878a81adeb75d49ba9965
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-08-11 18:21:51 +0100

    Updates

--- Source/Page.cpp
+++ Source/Page.cpp
@@ -1,33 +1,17 @@
 /*
     Copyright (c) 2018-2022 Xavier Leclercq
-
-    Permission is hereby granted, free of charge, to any person obtaining a
-    copy of this software and associated documentation files (the "Software"),
-    to deal in the Software without restriction, including without limitation
-    the rights to use, copy, modify, merge, publish, distribute, sublicense,
-    and/or sell copies of the Software, and to permit persons to whom the
-    Software is furnished to do so, subject to the following conditions:
-
-    The above copyright notice and this permission notice shall be included in
-    all copies or substantial portions of the Software.
-
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-    IN THE SOFTWARE.
+    Released under the MIT License
+    See https://github.com/diplodocusdb/physical-storage/blob/main/LICENSE.txt
 */
 
 #include "Page.h"
 #include "PageFileRepository.h"
 #include "PageRepositoryErrorCategory.hpp"
 #include <Ishiko/Errors.hpp>
+#include <Ishiko/IO.hpp>
 #include <sstream>
 
-namespace DiplodocusDB
-{
+using namespace DiplodocusDB;
 
 Page::Page(size_t index)
     : m_index(index), m_dataSize(0), m_availableSpace(sm_size - sm_startMarkerSize - sm_endMarkerSize), m_nextPage(0)
@@ -158,6 +142,4 @@ void Page::read(std::istream& input, Ishiko::Error& error)
             m_nextPage = nextPage;
         }
     }
-}
-
 }
