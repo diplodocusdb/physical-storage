bd106668378b1ba928943f9812aca035546a25bd
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-08-12 20:46:05 +0100

    Add files

---
+++ include/DiplodocusDB/PhysicalStorage/PageFileRepository2.hpp
@@ -0,0 +1,10 @@
+/*
+    Copyright (c) 2022 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/diplodocusdb/physical-storage/blob/main/LICENSE.txt
+*/
+
+#ifndef GUARD_DIPLODOCUSDB_PHYSICALSTORAGE_PAGEFILEREPOSITORY2_HPP
+#define GUARD_DIPLODOCUSDB_PHYSICALSTORAGE_PAGEFILEREPOSITORY2_HPP
+
+#endif
---
+++ src/PageFileRepository2.cpp
@@ -0,0 +1,7 @@
+/*
+    Copyright (c) 2022 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/diplodocusdb/physical-storage/blob/main/LICENSE.txt
+*/
+
+#include "PageFileRepository2.hpp"
